d47c566865db055835651a56f15f4f121d9611ba
Rakshit-Chadgal <subservience0@gmail.com>
2026-05-02 00:50:41 +0530

    second commit

--- OOPs.py
+++ OOPs.py
@@ -1,1 +1,7 @@
-#Q1
+#Q1 Create student class that takes name & marks of 3 subjects as arguments in constructor. Then create a method to print the average.
+
+
+#Q2: Create Account class with 2 attributes - balance & account no.
+
+
+#Q3: Create methods for debit, credit & printing the balance.
