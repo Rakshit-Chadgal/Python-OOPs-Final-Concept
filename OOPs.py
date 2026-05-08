90ce545b132906fc6e60f27fdcd59a7b9d8f53c7
Rakshit-Chadgal <subservience0@gmail.com>
2026-05-08 18:30:08 +0530

    1st Commit

--- OOPs.py
+++ OOPs.py
@@ -1,5 +1,19 @@
 #Q1 Create student class that takes name & marks of 3 subjects as arguments in constructor. Then create a method to print the average.
+class Student:
+    def __init__ (self, name, marks):
+        self.name = name
+        self.marks = marks
 
+    def get_avg(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+        print("hi", self.name, "your avg score is:", sum/3)
+s1 = Student("Rakshit", [99, 46, 89])
+s1.get_avg()
+
+s1.name = "ironman"
+s1.get_avg()
 
 #Q2: Create Account class with 2 attributes - balance & account no.
 
