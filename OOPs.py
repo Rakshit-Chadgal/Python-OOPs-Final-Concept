0647238161fb536d544d4d329f3cf13ac60dd3bb
Rakshit-Chadgal <subservience0@gmail.com>
2026-05-10 10:23:50 +0530

    Part 1 Committed

--- OOPs.py
+++ OOPs.py
@@ -1,4 +1,5 @@
 #Q1 Create student class that takes name & marks of 3 subjects as arguments in constructor. Then create a method to print the average.
+'''
 class Student:
     def __init__ (self, name, marks):
         self.name = name
@@ -14,8 +15,33 @@ s1.get_avg()
 
 s1.name = "ironman"
 s1.get_avg()
+'''
 
 #Q2: Create Account class with 2 attributes - balance & account no.
+class Account:
+    def __init__(self, bal, acc):
+        self.balance = bal
+        self.account_no = acc
 
+    #debit method
+    def debit(self, amount):
+        self.balance =- amount
+        print("Rs.", amount, "was debited")
+        print("total balance = ", self.get_balance())
+    
+    #credit method
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs.", amount, "was credited")
+        print("Total Balance = ", self.get_balance())
+
+    def get_balance(self):
+        print("Your balance is Rs.", self.balance)
+
+acc1 = Account(10000, 12345)
+acc1.credit(40000) #salary
+acc1.debit(1000) #shopping
+acc1.credit(500) #increment
+acc1.debit(10000) #rent
 
 #Q3: Create methods for debit, credit & printing the balance.
